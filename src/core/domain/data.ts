77e5369d4c5e538da3d0b9b15a097ee628a6edd2
ergunDevv <ergncyln2127088@gmail.com>
2023-02-21 16:39:50 +0300

    Core part added and domain part added

---
+++ src/core/domain/data.ts
@@ -0,0 +1,2 @@
+// In normal we  should define the data models that represent the entities and concepts in our application.
+// But because of our api sends us just info about games that's why i didn't add this part.
